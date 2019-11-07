414b116b932ed38698169c9cafd55e6ebcaeaeba
speater306 <speater306@gmail.com>
2019-11-07 11:06:33 +0800

    修改206題註解

--- lc206.java
+++ lc206.java
@@ -19,26 +19,12 @@ class Solution{
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
-        int iter = 1;
 
         while(curr != null){
-            System.out.printf("----Round%s----%n",iter);
             ListNode nextTmp = curr.next;
-            System.out.print("n:");
-            showlist(nextTmp);
-            System.out.println("");
             curr.next = prev;
-            System.out.print("c:");
-            showlist(curr);
-            System.out.println("");
             prev = curr;
-            System.out.print("p:");
-            showlist(prev);
-            System.out.println("");
             curr = nextTmp;
-            System.out.print("c:");
-            showlist(curr);
-            System.out.println("");
             iter++;
         }
         return prev;
@@ -62,7 +48,5 @@ public class lc206{
         }
         Solution sol = new Solution();
         sol.reverseList(test);
-        //Solution.showlist(test);
-        //System.out.println("");
     }
 }
